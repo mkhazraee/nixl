88659de1d65fb92a70fadd55e5dbc4f4723d0fde
Moein <moein@nvidia.com>
2025-08-29 06:27:24 +0300

    Addressing comments.

--- .gitlab/test_cpp.sh
+++ .gitlab/test_cpp.sh
@@ -78,9 +78,9 @@ cd ${INSTALL_DIR}
 ./bin/nixl_etcd_example
 ./bin/ucx_backend_test
 ./bin/ucx_mo_backend_test
-NIXL_TELEMETRY_ENABLE=y NIXL_TELEMETRY_DIR=/tmp ./bin/agent_example &
+NIXL_TELEMETRY_ENABLE=y NIXL_TELEMETRY_DIR=$PWD/telemetry ./bin/agent_example &
 sleep 1
-./bin/telemetry_reader /tmp/Agent001 &
+./bin/telemetry_reader $PWD/telemetry/Agent001 &
 telePID=$!
 sleep 6
 kill -s SIGINT $telePID
